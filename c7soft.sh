8ab24ba40be4fdf286d393f922f98a0cf12105a4
xinqiangc <xinqiangc@gmail.com>
2020-09-16 17:44:17 +0800

    modify xen64up.sh

--- c7soft.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-yum install vim wget curl net-tools ntpdate lrzsz bash-completion
--- sh/xen65up.sh
+++ sh/xen65up.sh
@@ -4,16 +4,18 @@ mount -t cifs -o password="" //172.21.23.201/GuestUpload/IT-team/patch/xen6.5 /m
 
 cd /mnt 
 
-xe patch-upload file-name=XS65ESP1.xsupdate > /tmp/uuid.txt
+xe patch-upload file-name=XS65ESP1.xsupdate &> /tmp/a.txt
 echo "XS65ESP1.xsupdate is uploaded."
+cat /tmp/a.txt |grep "uuid"|awk '{print $2}' > /tmp/uuid.txt
 xe patch-pool-apply uuid=`cat /tmp/uuid.txt`
 echo "XS65ESP1.xsupdate is installed."
 
 for filename in `ls /mnt`
 do 
 	if [[ $filename != "XS65ESP1.xsupdate" ]];then
-		xe patch-upload file-name=$filename > /tmp/uuid.txt
+		xe patch-upload file-name=$filename &> /tmp/a.txt
 		echo "$filename is uploaded."
+		cat /tmp/a.txt |grep "uuid"|awk '{print $2}' > /tmp/uuid.txt
 		xe patch-pool-apply uuid=`cat /tmp/uuid.txt`
 		echo "$filename is installed."
 	fi
